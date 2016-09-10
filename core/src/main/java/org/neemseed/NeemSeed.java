32dbb16fc2f8c8d578262f12925fc11d77f55227
Nova <nova@francium.tech>
2016-09-10 12:23:31 +0230

    Added core module

---
+++ core/src/main/java/org/neemseed/NeemSeed.java
@@ -0,0 +1,15 @@
+package org.neemseed;
+
+
+public class NeemSeed {
+
+    public static void main(String[] args){
+        NeemSeed neemseed = new NeemSeed();
+        neemseed.run();
+    }
+
+    public void run(){
+
+    }
+
+}
